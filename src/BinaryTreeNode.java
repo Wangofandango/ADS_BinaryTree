0763e96d4ce22a18a37e91616df3088a2cee3b54
Milo <miloamadsen@gmail.com>
2023-04-12 14:30:26 +0200

    Initial Commit as fuck

---
+++ src/BinaryTreeNode.java
@@ -0,0 +1,45 @@
+public class BinaryTreeNode<T>
+{
+  private T element;
+  private BinaryTreeNode<T> leftChild;
+  private BinaryTreeNode<T> rightChild;
+
+
+
+  public BinaryTreeNode(T element)
+  {
+    this.element = element;
+    leftChild = null;
+    rightChild = null;
+  }
+
+  public void setElement(T element)
+  {
+    this.element = element;
+  }
+
+  public T getElement()
+  {
+    return element;
+  }
+
+  public void addLeftChild(BinaryTreeNode<T> leftChild)
+  {
+    this.leftChild = leftChild;
+  }
+
+  public void addRightChild(BinaryTreeNode<T> rightChild)
+  {
+    this.rightChild = rightChild;
+  }
+
+  public BinaryTreeNode<T> getLeftChild()
+  {
+    return leftChild;
+  }
+
+  public BinaryTreeNode<T> getRightChild()
+  {
+    return rightChild;
+  }
+}
